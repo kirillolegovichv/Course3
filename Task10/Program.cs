adfe767a67bd0797e8cefb286ef829cec2c328b1
Kirill <kirill.vasil.oleg@gmail.com>
2022-03-06 21:10:35 +0300

    Change Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,8 +1,15 @@
 ﻿Console.Write(" Enter А: ");
 int A = Convert.ToInt32(Console.ReadLine());
+int result = 0;
 
 while(A > 0)
 {
-    Console.Write($"{A % 10}");
+    result += A % 10;
     A /= 10;
+    if (A != 0)
+    {
+        result *= 10;
+    }
 }
+
+Console.WriteLine(result);
