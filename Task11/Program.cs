e7b86b97da9703d3679fdfe0e389e1c175f5e303
Kirill <kirill.vasil.oleg@gmail.com>
2022-03-06 21:17:26 +0300

    Change Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,25 +1,23 @@
 ﻿Console.Write(" Enter N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int odd = 0;
-int even = 0;
-int j = 0;
+int tmp = 0;
 
 for (int i = 1; i < N; i++)
 {
-    j = i;
-    even = 0;
-    odd = 0;
-    while (j > 0)
+    tmp = i;
+    int odd = 0;
+    int even = 0;
+    while (tmp > 0)
     {
-        if ((j % 10) % 2 == 0)
+        if ((tmp % 10) % 2 == 0 && (tmp % 10) != 0)
         {
-            even += i;
+            even += tmp;
         }
         else
         {
-            odd += i;
+            odd += tmp;
         }
-        j /= 10;
+        tmp /= 10;
     }
     if (even > odd)
     {
